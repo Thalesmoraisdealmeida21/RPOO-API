ad84435312dd764a8543314d27afaa2223ae2546
Thales Morais <thales.morais21@gmail.com>
2019-11-11 20:14:28 -0300

    Ajusta config banco de dados

--- config/config.js
+++ config/config.js
@@ -9,5 +9,15 @@ module.exports = {
         database: "db_api_rpg",
 
     },
+    production: {
+
+        host: '35.222.248.99',
+        secret: '1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3',
+        dialect: 'postgres',
+        username: "postgres",
+        password: "bFxA7sr10Naf1htO",
+        database: "db_api_rpg",
+
+    },
  
 };
