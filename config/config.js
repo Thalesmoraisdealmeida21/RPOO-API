aabe3fac60d4cc5fa88475db1674de54a1751c45
Thales Morais <thales.morais21@gmail.com>
2019-11-11 21:47:03 -0300

    altera servidor

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     development: {
 
-        host: '35.222.248.99',
+        host: "mysql.thalesmoras.kinghost.net",
         secret: '1C3C7E1694F1E9DAD939399E87E5FFB5DF06B2327CA31B409CB3',
-        dialect: 'postgres',
-        username: 'postgres',
-        password: "1234",
-        database: "db_api_rpg",
+        dialect: 'mysql',
+        username: 'thalesmoras',
+        password: "ta211097",
+        database: "thalesmoras",
 
     },
  
